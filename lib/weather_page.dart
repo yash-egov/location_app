d9d2a0485afa97a2af1bd4f33ee2ddfdcc7a53aa
yash-egov <yash.meena@egovernments.org>
2024-07-25 12:07:48 +0530

    completed

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -109,7 +109,7 @@ class _WeatherPageScreenState extends State<WeatherPageScreen> {
   @override
   Widget build(BuildContext context) {
     Future<void> _delayedBuild() async {
-      await Future.delayed(const Duration(seconds: 2));
+      await Future.delayed(const Duration(seconds: 1));
     }
 
     return Scaffold(
